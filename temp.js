bdc06220d8be31f4e3a0206b9ea234f3eccc9005
amritha-00 <amrithavemuri@gmail.com>
2024-11-21 18:29:55 +0530

    okey

---
+++ temp.js
@@ -0,0 +1,18 @@
+db.Products.insertMany(
+    [
+    {
+        "name":"Product 7",
+        "desc":"This is the description of product 7",
+        "price":200,
+        "url":"https://picsum.photos/id/7/200/300"
+
+    },
+    {
+        "name":"Product 8",
+        "desc":"This is the description of product 8",
+        "price":330,
+        "url":"https://picsum.photos/id/8/200/300"
+
+    },
+]
+);
